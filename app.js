230c086028e3d3b8cee12553991b3099683267c5
Danilo Braband <d.braband@k-newmedia.de>
2012-05-03 13:50:36 +0200

    Set package to private and abstract web.js.

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var strata = require("strata"),
     path = require("path")
 
 strata.use(strata.commonLogger)
-strata.use(strata.contentType, "text/plain")
 strata.use(strata.contentLength)
+strata.use(strata.contentType, "text/plain")
 strata.use(strata.file, path.join(__dirname, "public"))
 
 strata.get("/", function (env, callback) {
     callback(200, {}, "Hello world!")
 })
 
-strata.run()
+module.exports = strata.app
